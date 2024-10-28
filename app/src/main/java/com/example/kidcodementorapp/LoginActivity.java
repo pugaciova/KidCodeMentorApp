890df903add729ea33686739f9f75a4bba0d94b5
pugaciova <inna.pugaciova@isa.utm.md>
2024-10-28 09:24:00 +0200

    added registration activity and modified login page

--- app/src/main/java/com/example/kidcodementorapp/LoginActivity.java
+++ app/src/main/java/com/example/kidcodementorapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.kidcodementorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login); // Устанавливаем существующий layout файл
+
+        // Найдите текст "Еще нет аккаунта? Зарегистрироваться"
+        TextView signupText = findViewById(R.id.signupText);
+
+        // Установите обработчик нажатия
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Запустите RegisterActivity
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
