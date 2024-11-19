c68a6d937b23e86713bfa34e08f5202ecd53869a
pugaciova <inna.pugaciova@isa.utm.md>
2024-11-19 22:46:28 +0200

    add category of courses page

--- app/src/main/java/com/example/kidcodementorapp/AboutActivity.java
+++ app/src/main/java/com/example/kidcodementorapp/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.example.kidcodementorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,23 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about);
+
+        // Обработка системных отступов
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Найти кнопку "Начнем" и установить обработчик нажатия
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создать Intent для перехода к CategoryActivity
+                Intent intent = new Intent(AboutActivity.this, CategoryActivity.class);
+                startActivity(intent);  // Запустить CategoryActivity
+            }
         });
     }
 }
